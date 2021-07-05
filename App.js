8d448ad2904e0c62f6106df349c11ca860be4367
lxghan <lex004@udayton.edu>
2021-07-05 20:57:23 +0700

    update App.js: added Main page

--- App.js
+++ App.js
@@ -11,6 +11,129 @@ let searchInp = "";
 let searchInp2 = "";
 let url = "https://appmockapi.herokuapp.com/author/search";
 
+const Main = ({navigation}) => {
+  return (
+  <View>
+  <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="description" content="" />
+          <meta name="author" content="" />
+          <title>swj</title>
+          <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+          <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+
+          <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+          <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+          <link href="/assets/css/styles.css" rel="stylesheet" />
+
+  </head>
+    <div id="page-top">
+          <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+              <div class="container px-4 px-lg-5">
+                  <a class="navbar-brand" href="#page-top">SWJ</a>
+                  <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                      Menu
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarResponsive">
+                      <ul class="navbar-nav ms-auto">
+                          <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                          <li class="nav-item"><a class="nav-link" href="#contributors">Contributors</a></li>
+                          <li class="nav-item"><a class="nav-link" href="#references">References</a></li>
+                      </ul>
+                  </div>
+              </div>
+          </nav>
+
+          <header class="masthead">
+              <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                  <div class="d-flex justify-content-center">
+                      <div class="text-center">
+                          <h1 class="mx-auto my-0 text-uppercase">Society of Women Journalists</h1>
+                          <h2 class="text-white-50 mx-auto mt-2 mb-5">Brief introduction about SWJ website</h2>
+                          <a class="btn btn-primary" href="#about">Get Started</a>
+                      </div>
+                  </div>
+              </div>
+          </header>
+
+          <section class="about-section text-center" id="about">
+              <div class="container px-4 px-lg-5">
+                  <div class="row gx-4 gx-lg-5 justify-content-center">
+                      <div class="col-lg-8">
+                          <h2 class="text-white mb-4">About Us</h2>
+                          <p class="text-white-50">
+                              About Us section
+                          </p>
+                      </div>
+                  </div>
+                  <img class="img-fluid" src="../assets/css/img/bg-masthead.jpg" alt="..." />
+              </div>
+          </section>
+
+          <section class="projects-section bg-light" id="contributors">
+              <div class="container px-4 px-lg-5">
+
+              <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                <div class="col-lg-6"><img class="img-fluid" src="../assets/css/img/demo-image-01.jpg" alt="..." /></div>
+                <div class="col-lg-6">
+                    <div class="bg-black text-center h-100 project">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                                <h4 class="text-white">Contributors</h4>
+                                <p class="mb-0 text-white-50">Contributors section</p>
+                                <hr class="d-none d-lg-block mb-0 ms-0" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                  <div class="row gx-0 justify-content-center">
+                      <div class="col-lg-6"><img class="img-fluid" src="../assets/css/img/demo-image-02.jpg" alt="..." /></div>
+                      <div class="col-lg-6 order-lg-first">
+                          <div class="bg-black text-center h-100 project">
+                              <div class="d-flex h-100">
+                                  <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <Text style={styles.searchNav}
+                                      onPress ={() => {
+                                        navigation.navigate('Search')
+                                      }}>Search</Text>
+                                      <p class="mb-0 text-white-50">Search for different journalists by their names, pen names, and leadership positions</p>
+                                      <hr class="d-none d-lg-block mb-0 me-0" />
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+
+          <section class="references-section" id="references">
+              <div class="container px-4 px-lg-5">
+                  <div class="row gx-4 gx-lg-5">
+                      <div class="col-md-10 col-lg-8 mx-auto text-center">
+                          <h2 class="text-white mb-5">References</h2>
+                          <p class="text-white mb-5">
+                          References section
+                          </p>
+                      </div>
+                  </div>
+                </div>
+          </section>
+
+
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+
+          <script src="/assets/js/scripts.js"></script>
+          <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Society of Women Journalists 2021</div></footer>
+      </div>
+  </View>
+  )
+}
 
 const Search = ({navigation}) => {
   const [name, onChangeFname] = React.useState();
@@ -311,6 +434,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main}/>
         <Stack.Screen name="Search" component={Search}/>
         <Stack.Screen name="Search Results" component={SearchRes}/>
         <Stack.Screen name="Biography" component={Bio}/>
@@ -334,6 +458,10 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 30,
     fontWeight: "bold",
+    color: "white",
+  },
+  searchNav: {
+    fontSize: 25,
     color: "white",
   },
   searchInst: {
